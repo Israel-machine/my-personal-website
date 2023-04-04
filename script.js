473806f4e207e96de85e17c59e868084d916d6f5
israel diaz <israel.diaz1701@gmail.com>
2023-04-04 00:03:48 -0700

    j

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+let dblClickButton = document.querySelector(".funButton");
+let body=document.querySelector("body");
+
+dblClickButton.onclick= function(){
+    body.style.backgroundColor="pink";
+};
